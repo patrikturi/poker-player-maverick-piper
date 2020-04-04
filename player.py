8d3d29d6fc0ea1b1b238a92df793213c20e2f94a
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-04-04 14:58:53 +0200

    Add logging

--- player.py
+++ player.py
@@ -12,17 +12,43 @@ class Player:
         current_buy_in=game_state['current_buy_in']
 
         cards = me['hole_cards']
+        self.rank1 = None
+        self.rank2 = None
         if len(cards) > 1:
+            self.rank1 = cards[0]['rank']
+            self.rank2 = cards[1]['rank']
             value = chen.get_value(cards)
-            if value <= 10 and cards[0]['rank'] != 'A' and cards[1]['rank'] != 'A':
-                return 0 # call only if no raise
+            if value <= 10 and self.rank1 != 'A' and self.rank2 != 'A':
+                self.log_check()
+                return 0 # check only, no raise
             if cards[0]['rank'] == cards[1]['rank']:
+                self.log_allin()
                 return 1000 # all in
 
         minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
         r = minimum_raise if random.random()>0.7 else 0
 
-        return current_buy_in - me['bet'] + r # always call
+        amount = current_buy_in - me['bet'] + r
+        self.log_call(amount)
+        return amount # always call
 
     def showdown(self, game_state):
         return True
+
+    def log_check(self):
+        if self.rank1:
+            print('{} {}: CHECK only'.format(self.rank1, self.rank2))
+        else:
+            print('CHECK only')
+
+    def log_call(self, amount):
+        if self.rank1:
+            print('{} {}: CALL: {}'.format(self.rank1, self.rank2, amount))
+        else:
+            print('CALL: {}'.format(amount))
+
+    def log_allin(self):
+        if self.rank1:
+            print('{} {}: ALL IN'.format(self.rank1, self.rank2))
+        else:
+            print('ALL IN')
