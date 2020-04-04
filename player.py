113e0d7eced615d5e2e80fd9abb82c90c3c2fbc4
n3ko <n3ko.74@gmail.com>
2020-04-04 13:10:47 +0200

    tested

--- player.py
+++ player.py
@@ -7,8 +7,9 @@ class Player:
         in_action=game_state['in_action']
         me=game_state['players'][in_action]
         current_buy_in=game_state['current_buy_in']
-        return current_buy_in - me['bet']
-#	+ 'minimum_raise'
+        minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
+        r = minimum_raise if random.random()>0.7 else 0
+        return current_buy_in - me['bet'] + r
 
     def showdown(self, game_state):
         return True
