97c668122aec9d4000b51a252ce46f2772db1f04
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-04-04 13:26:11 +0200

    Fold if card value is low

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import random
+import chen
 
 class Player:
     VERSION = "Default Python folding player"
@@ -9,8 +10,12 @@ class Player:
         current_buy_in=game_state['current_buy_in']
 
         cards = me['hole_cards']
-        if len(cards) > 1 and cards[0]['rank'] == cards[1]['rank']:
-            return 1000
+        if len(cards) > 1:
+            if cards[0]['rank'] == cards[1]['rank']:
+                return 1000
+            value = chen.get_value(cards)
+            if value <= 7:
+                return me['bet'] # call
 
         minimum_raise = game_state['minimum_raise'] if 'minimum_raise' in game_state else 0
         r = minimum_raise if random.random()>0.7 else 0
