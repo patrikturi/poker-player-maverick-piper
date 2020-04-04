249d8bc90192d52f726d6375bd3bf8d26e69731c
n3ko <n3ko.74@gmail.com>
2020-04-04 11:27:07 +0200

    random

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+import random
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 1000
+        return int(random.random()*1000)
 
     def showdown(self, game_state):
         return True
