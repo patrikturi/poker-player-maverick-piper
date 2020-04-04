7c8ba559feb3c21b01e06115ac66eaeb1c027b43
n3ko <n3ko.74@gmail.com>
2020-04-04 16:18:29 +0200

    checkonlytest

--- test_bet.py
+++ test_bet.py
@@ -16,3 +16,7 @@ class BetTestCase(unittest.TestCase):
                 'suit': 'diamonds', 'rank': 'K'
              }], 'time_used': 842363, 'stack': 752, 'bet': 105, 'status': 'active', 'name': 'Maverick Piper'}]}
         self.assertEqual(30, Player().betRequest(gs))
+    def test_whyraise(self):
+        gs={u'players': [{u'time_used': 1757056, u'version': u'Default Java folding player', u'id': 0, u'status': u'out', u'name': u'I have a JavaScript job opportunity for you', u'bet': 0, u'stack': 0}, {u'time_used': 2481701, u'version': u'Post flops are cool', u'id': 1, u'status': u'active', u'name': u'AllLean', u'bet': 300, u'stack': 2362}, {u'time_used': 4573021, u'version': u'Default Python folding player', u'id': 2, u'status': u'active', u'name': u'Maverick Piper', u'hole_cards': [{u'suit': u'diamonds', u'rank': u'10'}, {u'suit': u'diamonds', u'rank': u'8'}], u'stack': 188, u'bet': 150}], u'in_action': 2, u'tournament_id': u'5e7f0ed0448e31000456e3d0', u'minimum_raise': 150, u'big_blind': 300, u'pot': 450, u'current_buy_in': 300, u'dealer': 1, u'small_blind': 150, u'game_id': u'5e8894530a5c770004c109da', u'round': 119, u'orbits': 39, u'bet_index': 3, u'community_cards': []}
+        self.assertEqual(0, Player().betRequest(gs))
+
