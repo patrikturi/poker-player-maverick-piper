40459a3626f74b3b18f99f15d758ffc49670e444
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-04-04 10:44:41 +0200

    All in

--- player.py
+++ player.py
@@ -3,8 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        return 1000
 
     def showdown(self, game_state):
-        pass
+        return True
 
