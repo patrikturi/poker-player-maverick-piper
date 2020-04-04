8a79b1d74c115c6adc50a49ff82068cc6202c6e7
n3ko <n3ko.74@gmail.com>
2020-04-04 11:28:56 +0200

    hold

--- player.py
+++ player.py
@@ -4,7 +4,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return int(random.random()*1000)
+        return game_state['current_buy_in'] - game_state['players']['in_action']['bet']
+#	+ 'minimum_raise'
 
     def showdown(self, game_state):
         return True
